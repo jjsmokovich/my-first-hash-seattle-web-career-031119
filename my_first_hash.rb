5c378bad93ce96f4023b8ed0f044598c1e3d13b1
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-05 07:38:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,8 +29,8 @@ def adding
               
                     }
   
-  shipping_manifest["muskets"] = 4
-  shipping_manifest["gunpowder"] = 2
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   
  return shipping_manifest
 end
