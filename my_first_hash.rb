4ae54d9eddd95881582ecb9a4daa0eb200bbbe85
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-05 07:29:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,10 +29,8 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4
-          
-                   }
+              
+                    }
   # add 2 muskets to the shipping_manifest hash below
 
 
@@ -43,6 +41,6 @@ def adding
 
   # return the shipping_manifest hash below
   
- 
+ return shipping_manifest
 end
  
